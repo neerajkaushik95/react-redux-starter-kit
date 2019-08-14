f9116d37bd42592a8a704a3209e577b9afd525e1
neerajkaushik95 <neeraj.kaushik95@gmail.com>
2019-08-14 13:31:26 +0530

    Injected common example reducer to redux state

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 import counterReducer from 'modules/Counter/counterReducer';
+import authReducer from 'shared/reducers/auth';
 
 import history from './history';
 
 const rootReducer = combineReducers({
   router: connectRouter(history),
   counter: counterReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
